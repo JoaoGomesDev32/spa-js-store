ad9d026c51ef1ad631c39504c1675ece2db2686f
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-07-12 10:47:26 +0100

    create global styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Home from "./pages/Home/Home.jsx";
+import { GlobalStyled } from "./GlobalStyled.jsx";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <GlobalStyled />
+      <Home />;
+    </>
+  );
 }
 
 export default App;
